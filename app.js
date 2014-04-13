5c0bcbde8b174b7c122f36a49b50e846e808a319
hajipion <hajiming@gmail.com>
2014-04-13 17:11:08 +0900

    うわーーーー

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ function cloneRepository(){
 
 function showCreateWindow(){
 	var cWindow = $(
-		'<div id="create-window"><ul>'
+		'<div class="create-window"><ul>'
 		+	'<li>リポジトリ名：<input type="text" name="repository"></li>'
 		+	'<li>ブックマーク名：<input type="text" name="bookmark"></li>'
 		+	'<li>保存先のパス：<input type="text" name="path"></li>'
 		+	'</ul>'
 		+ 	'<p><input type="button" value="キャンセル">'
 		+	'<input type="submit" value="新規作成"></p></div>');
-	var win = document.getElementById("menu");
+	var win = document.getElementById("main");
 	cWindow.appendTo(win);
 }
