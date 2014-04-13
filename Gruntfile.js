bc280bf24996fb1462d9afbf396973e403ddf209
Daniel Perez <tuvistavie@gmail.com>
2014-04-13 09:44:49 +0900

    Initial commit.

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+/*global module:false*/
+module.exports = function (grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    // Task configuration.
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        globals: {
+        }
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      }
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      }
+    },
+    exec: {
+      nw: {
+        command: 'nw .'
+      }
+    },
+    concurrent: {
+      dev: {
+        tasks: ['exec:nw', 'watch'],
+        options: {
+          logConcurrentOutput: true
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-exec');
+  grunt.loadNpmTasks('grunt-concurrent');
+
+  // Default task.
+  grunt.registerTask('default', ['concurrent:dev']);
+};
